ff88cd78f925cabdb10fb86cf48a748429afbc1a
Edison David González Blandon <Edison David Gonzlez Blandon>
2016-08-24 15:55:43 -0500

    Initial commit

---
+++ app/src/main/java/com/edisongonzlez/practicaclase/Actividad_Clase1.java
@@ -0,0 +1,13 @@
+package com.edisongonzlez.practicaclase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Actividad_Clase1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad__clase1);
+    }
+}
